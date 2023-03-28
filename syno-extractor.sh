6f7575b24800a39cc7873ec94f5cd7c1dca25fb5
Ing <wjz304@qq.com>
2023-03-29 02:36:08 +0800

    rm dev

--- syno-extractor.sh
+++ syno-extractor.sh
@@ -3,8 +3,8 @@
 ROOT_PATH=$PWD
 TOOL_PATH="$(dirname $(readlink -f "$0"))/syno-extractor"
 
-# GITHUB_URL="https://raw.githubusercontent.com/wjz304/Redpill_CustomBuild/main/syno-extractor"
-GITHUB_URL="https://fastly.jsdelivr.net/gh/wjz304/Redpill_CustomBuild@main/syno-extractor"
+# GITHUB_URL="https://raw.githubusercontent.com/wjz304/Redpill_CustomBuild_old/main/syno-extractor"
+GITHUB_URL="https://fastly.jsdelivr.net/gh/wjz304/Redpill_CustomBuild_old@main/syno-extractor"
 [ ! -d "${TOOL_PATH}" ] && mkdir -p "${TOOL_PATH}"
 for f in libcurl.so.4 libmbedcrypto.so.5 libmbedtls.so.13 libmbedx509.so.1 libmsgpackc.so.2 libsodium.so libsynocodesign-ng-virtual-junior-wins.so.7 syno_extract_system_patch; do
   [ ! -e "${TOOL_PATH}/${f}" ] && curl -L "${GITHUB_URL}/${f}" -o "${TOOL_PATH}/${f}"
